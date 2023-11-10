e3646a6eb1102e3f83d08a610c1829c1cb077429
unaispk <unaispk46@gmail.com>
2023-11-10 22:52:36 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from "react";
 import TodoApp from "./component/TodoApp/TodoApp";
+import About from "./component/About/About";
+import Header from "./component/Header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+export default () => {
+  return (
 
- const App = () => <TodoApp/>;
- export default App;
+    <Router>
+    <Header />
+    <Routes>
+      <Route path="/" exact element={<TodoApp />} />
+
+      <Route path="/About" Component={About}/>
+    </Routes>
+  </Router>
+
+  );
+};
+

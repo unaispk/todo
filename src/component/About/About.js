50d4b8c4800b191fbeed87a5ae60723e1f1b280a
unaispk <unaispk46@gmail.com>
2023-11-11 12:31:48 +0530

    About section content added

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
     <div>
-      <h1 style={{color:"white", textAlign:"center"}}>About Component</h1>
+      <div className="about-p">
+      
+        <p>
+        Welcome to our simple and efficient todo app, crafted with care by Muhammed Unais P K as part of his React.js learning journey. This app is designed to enhance your daily productivity by helping you keep track of tasks effortlessly.
+        </p>
+        <h4>Key Features:</h4>
+        <p><b>User-friendly Interface:</b> Our todo app boasts a clean and intuitive design, making task management a breeze.</p>
+        <p><b>Delete Functionality:</b> Easily remove completed tasks with the click of a button, ensuring a clutter-free list.</p>
+        <p><b>Refresh without Loss:</b> Enjoy the convenience of refreshing the app without losing your saved tasks.</p>
+        <p>Feel free to utilize this app for your daily needs or any other purpose that requires task organization. Join us in simplifying your day-to-day life through effective task management.</p>
+        <p>Created with passion and dedication,<br />Muhammed Unais P K</p>
+      </div>
     </div>
   );
 };
